e491385664423470bc23828859a3b0455a898847
Son Minh <minh.huynhcao@gmail.com>
2022-05-07 09:22:28 +0700

    220507

---
+++ lab02_2/src/components/DishdetailComponent.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
+
+class DishDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedDish: null,
+    };
+  }
+
+  onDishSelect(dish) {
+    this.setState({ selectedDish: dish });
+  }
+
+  renderDish(dish) {
+    if (dish != null) {
+      return (
+        <div key={dish.id} className="col-12 col-md-5 m-1">
+          <Card>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardBody>
+              <CardTitle>{dish.name}</CardTitle>
+              <CardText>{dish.description}</CardText>
+            </CardBody>
+          </Card>
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
+
+  renderComments(dish) {
+    if (dish != null) {
+      return (
+        <div key={dish.id} className="col-12 col-md-5 m-1">
+          <h4>Comments</h4>
+          <ul className="list-unstyled">
+            {dish.comments.map((comment) => {
+              return (
+                <li>
+                  <p>{comment.comment}</p>
+                  <p>
+                    --{comment.author},{" "}
+                    {new Intl.DateTimeFormat("en-US", {
+                      year: "numeric",
+                      month: "short",
+                      day: "2-digit",
+                    }).format(new Date(Date.parse(comment.date)))}
+                  </p>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
+
+  render() {
+    const menus = this.props.dishes.map((dish) => {
+      return (
+        <div key={dish.id} className="col-12 col-md-5 m-1">
+          <Card onClick={() => this.onDishSelect(dish)}>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImgOverlay>
+              <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+          </Card>
+        </div>
+      );
+    });
+
+    return (
+      <div>
+        <div className="container">
+          <div className="row">{menus}</div>
+          <div className="row">{this.renderDish(this.state.selectedDish)}</div>
+          <div className="row">
+            {this.renderComments(this.state.selectedDish)}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default DishDetail;
